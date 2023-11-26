ce012d72859b4e4feccc0c3730b0c91c59e35f17
Jess Frazelle <github@jessfraz.com>
2023-11-26 12:14:49 -0800

    updates
    Signed-off-by: Jess Frazelle <github@jessfraz.com>

--- build.rs
+++
@@ -1,7 +0,0 @@
-use cuda_builder::CudaBuilder;
-
-fn main() {
-    CudaBuilder::new(std::env::current_dir().unwrap())
-        .build()
-        .unwrap();
-}
--- src/add.rs
+++ src/add.rs
@@ -1,6 +1,7 @@
 use cuda_std::*;
 
 #[kernel]
+#[allow(clippy::missing_safety_doc)]
 pub unsafe fn add(a: &[f32], b: &[f32], c: *mut f32) {
     let idx = thread::index_1d() as usize;
     if idx < a.len() {
